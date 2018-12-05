2edc9e935f5395e8db0ecc6e748fadf44398f8b6
yangchong211 <yangchong211@163.com>
2018-12-05 20:44:12 +0800

    更新代码

---
+++ venv/Include/Config.py
@@ -0,0 +1,32 @@
+# -*-coding:utf-8-*-
+
+# keystore信息，注意这里的信息需要自己填写
+# Windows 下路径分割线请注意使用\\转义
+# keystorePath = "your.keystore"
+# keyAlias = "your keyAlias"
+# keystorePassword = "your keystorePassword"
+# keyPassword = "your keyPassword"
+
+# keystore信息
+# Windows 下路径分割线请注意使用\\转义
+keystorePath = "D:\\GitHub\\YCWalleHelper\\venv\\Include\\apk\\ycPlayer.jks"
+keyAlias = "yc"
+keystorePassword = "19930211"
+keyPassword = "19930211"
+
+
+
+# 加固后的源文件名（未重签名）
+protectedSourceApkName = "app_release.apk"
+# 加固后的源文件所在文件夹路径(...path),注意结尾不要带分隔符，默认在此文件夹根目录
+protectedSourceApkDirPath = ""
+# 渠道包输出路径，默认在此文件夹output目录下
+channelsOutputFilePath = ""
+# 渠道名配置文件路径，默认在此文件夹apk目录下
+channelFilePath = ""
+# 额外信息配置文件（绝对路径）
+# 配置信息示例参看，默认是此文件夹apk目录下
+extraChannelFilePath = ""
+# Android SDK buidtools path , please use above 25.0+
+# sdkBuildToolPath = "/Users/mac/Library/Android/sdk/build-tools/26.0.2"
+sdkBuildToolPath = "D:\\Program File\\AndroidSdk\\build-tools\\28.0.3"
